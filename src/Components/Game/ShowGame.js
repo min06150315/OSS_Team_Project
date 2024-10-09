acbfe557a2ba9c1be6c02e4b3b50268c37a7f710
min06150315 <min06150315@gmail.com>
2024-10-09 21:02:08 +0900

    Update ShowGame

--- src/Components/Game/ShowGame.js
+++ src/Components/Game/ShowGame.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Loader from "../Common/Loader"
+import Loader from "../Common/Loader";
 import "./ShowGame.css";
 
 const ShowGame = () => {
-    const showGameApi = "http://localhost:3000/api/games";
+    const showGameApi = "https://66ff38172b9aac9c997e8ee3.mockapi.io/api/games";
     const [game, setGame] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const handelDelete = async (id) => {
+    const handleDelete = async (id) => {
         console.log("id : -", id);
         setIsLoading(true);
         try {
@@ -44,54 +44,40 @@ const ShowGame = () => {
     };
 
     if (game.length < 0) {
-        return <h1>no game found</h1>;
+        return <h1>No game found</h1>;
     } else {
         return (
-            <div className="mt-5">
+            <div className="game-container">
                 {isLoading && <Loader />}
                 {error && <p>Error: {error}</p>}
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>게임 이름</th>
-                            <th>장르</th>
-                            <th>제작사</th>
-                            <th>출시년도</th>
-                            <th>플랫폼</th>
-                            <th>가격</th>
-                            <th>연령등급</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {game?.map((item, i) => {
-                            return (
-                                <tr key={i + 1}>
-                                    <td>{item.gameName}</td>
-                                    <td>{item.genre}</td>
-                                    <td>{item.developer}</td>
-                                    <td>{item.releaseYear}</td>
-                                    <td>{item.platform}</td>
-                                    <td>{item.price}</td>
-                                    <td>{item.ageRating}</td>
-                                    <td className="actions">
-                                        <Link to={`/edit-game/${item.id}`}>
-                                            <i className="fa fa-pencil" aria-hidden="true"></i>
-                                        </Link>
-                                        <Link to={`/game/${item.id}`}>
-                                            <i className="fa fa-gamepad" aria-hidden="true"></i>
-                                        </Link>
-                                        <i
-                                            className="fa fa-trash"
-                                            aria-hidden="true"
-                                            onClick={() => handelDelete(item.id)}
-                                        ></i>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                <div className="game-card-list">
+                    {game?.map((item, i) => (
+                        <div className="game-card" key={i}>
+                            <h2>{item.gameName}</h2>
+                            <p>Genre: {item.genre}</p>
+                            <p>Developer: {item.developer}</p>
+                            <p>Release Year: {item.releaseYear}</p>
+                            <p>Platform: {item.platform}</p>
+                            <p>Price: ${item.price}</p>
+                            <p>Age Rating: {item.ageRating}</p>
+                            <div className="actions">
+                                <Link to={`/edit-game/${item.id}`}>
+                                    <i className="fa-solid fa-pen-to-square" aria-hidden="true"></i>
+                                </Link>
+                                <Link to={`/game/${item.id}`}>
+                                    <i className="fa-solid fa-circle-info"></i>
+                                </Link>
+                                <Link>
+                                    <i
+                                        className="fa-solid fa-trash"
+                                        aria-hidden="true"
+                                        onClick={() => handleDelete(item.id)}
+                                    ></i>
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
