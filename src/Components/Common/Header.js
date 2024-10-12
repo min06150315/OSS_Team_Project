6079422dd94b20047a05966537da4290018798e1
min06150315 <min06150315@gmail.com>
2024-10-12 09:06:32 +0900

    Update Header.js

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Common.css';
 
-const Header = () => {
+const Header = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    onSearch(event.target.value); // 검색어를 부모 컴포넌트로 전달
+  };
+
   return (
-    <header className="header bg-light p-3">
-      <div className="container-fluid">
-        <input type="text" className="form-control" placeholder="Search..." />
-      </div>
+    <header className="header">
+      <input 
+        type="text" 
+        className="form-control" 
+        placeholder="검색하기" 
+        value={searchTerm}
+        onChange={handleSearch} // 입력 변경 시 핸들러 호출
+      />
     </header>
   );
 };
