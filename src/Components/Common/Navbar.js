a1366b241a977c492b87c19b6d091794f793ec53
min06150315 <min06150315@gmail.com>
2024-10-16 03:28:29 +0900

    Update Navbar.js

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Common.css';
+import React from 'react'; // React 라이브러리 import
+import { Link } from 'react-router-dom'; // Link 컴포넌트를 react-router-dom에서 import
+import './Common.css'; // CSS 파일 import
 
 const Navbar = () => {
   return (
-    <nav className="custom-navbar">
-      <ul className="navbar-nav">
-        <li className='nav-item'>
-          <Link to="/" className="navbar-brand nav-title" href="#">OSS Team Project</Link>
+    <nav className="custom-navbar"> {/* 사용자 정의 네비게이션 바 시작 */}
+      <ul className="navbar-nav"> {/* 네비게이션 항목 리스트 */}
+        <li className='nav-item'> {/* 네비게이션 아이템 */}
+          <Link to="/" className="navbar-brand nav-title" href="#">OSS Team Project</Link> {/* 홈으로 이동하는 링크 */}
         </li>
-        <div className='nav-content'>
+        <div className='nav-content'> {/* 네비게이션 내용 컨테이너 */}
           <li className="nav-item"> 
-            <Link className="nav-link" to="create-game">Create Game</Link>
+            <Link className="nav-link" to="create-game">Create Game</Link> {/* 게임 생성 페이지로 이동하는 링크 */}
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="view-table">View Table</Link>
+            <Link className="nav-link" to="view-table">View Table</Link> {/* 게임 테이블 보기 페이지로 이동하는 링크 */}
           </li>
         </div>
       </ul>
@@ -22,4 +22,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar; // Navbar 컴포넌트 내보내기
